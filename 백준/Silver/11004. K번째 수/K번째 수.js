e17060c7cf21732a325670d09246fc5be0ac6570
Subin <77488652+Siby1lA@users.noreply.github.com>
2023-07-07 20:26:11 +0900

    [Silver V] Title: K번째 수, Time: 4576 ms, Memory: 569252 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.js
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const stdin = fs.readFileSync('/dev/stdin').toString().trim();
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
-const [NK, ...A] = stdin.split("\n");
-const [_, K] = NK.split(" ").map(Number);
-const sortA = A[0].split(" ").map(Number).sort((a, b) => a - b);
+const [_, K] = stdin[0].split(" ").map(Number);
+const A = stdin[1].split(" ").map(Number);
+const sortA = A.sort((a, b) => a - b);
 console.log(sortA[K - 1]);
